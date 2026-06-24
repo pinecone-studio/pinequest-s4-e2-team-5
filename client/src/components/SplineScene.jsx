c121b56abdbe3e1dc1490d33eed7ba63fb4b8201
Administrator <munhorgilmunhtsetseg333@gmail.com>
2026-06-24 15:03:11 +0800

    Ai assistant-iin avatar iig tom har robot bolgoj oruulsan

---
+++ client/src/components/SplineScene.jsx
@@ -0,0 +1,19 @@
+import { Suspense, lazy } from 'react'
+
+const Spline = lazy(() => import('@splinetool/react-spline'))
+
+export const ROBOT_SCENE_URL = 'https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode'
+
+export function SplineScene({ scene = ROBOT_SCENE_URL, className = '' }) {
+  return (
+    <Suspense
+      fallback={
+        <div className="spline-loading" role="status" aria-label="3D робот ачаалж байна">
+          <span className="spline-loader" />
+        </div>
+      }
+    >
+      <Spline scene={scene} className={className} />
+    </Suspense>
+  )
+}
