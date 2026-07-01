fc6007cf328c092de7f3cfa0d222f4e2a9fc8f18
baterdene15 <altangerelbaterdene38@gmail.com>
2026-07-01 14:12:12 +0800

    switch avatar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,6 +42,9 @@ function App() {
   const navigate = (path, state = {}) => {
     stopAllAudio();
     window.history.pushState(state, "", path);
+    // next/navigation шимийн usePathname-г синк байлгана. Үгүй бол PageTransition-ий
+    // overlay буцаж хаагдахгүй үлдэж, дараагийн шилжилтэд дэлгэц харанхуйлж гацна.
+    window.dispatchEvent(new Event("pushstate-internal"));
     setPage(getPageFromPath(path));
   };
 
